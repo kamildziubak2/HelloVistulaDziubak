c05cd1dadd0fb0d3d465a3fe4d46e55b36d6c9bc
Kamil <kamildziubak19@gmail.com>
2022-10-06 15:50:46 +0200

    First commit

---
+++ src/pl/vistula/helloworldDziubak/HelloWorldDziubak.java
@@ -0,0 +1,9 @@
+package pl.vistula.helloworldDziubak;
+
+public class HelloWorldDziubak  {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Vistula Dziubak");
+    }
+}
