fc4fa2b25d52f7d8a368109c6ddbf89261417404
Kamil <kamildziubak19@gmail.com>
2022-10-06 15:55:25 +0200

    Added another lines

--- src/pl/vistula/helloworldDziubak/HelloWorldDziubak.java
+++ src/pl/vistula/helloworldDziubak/HelloWorldDziubak.java
@@ -5,5 +5,8 @@ public class HelloWorldDziubak  {
     public static void main(String[] args)
     {
         System.out.println("Hello Vistula Dziubak");
+        System.out.println("This is second line printed by this program");
+        System.out.println("This is another line");
+        System.out.println("And this is the last line");
     }
 }
